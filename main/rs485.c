47d714aed8bb2adbf19e9cb2b7ef31efb8582d17
Sotatek-TuHoang <tu.hoang@sotatek.com>
2023-10-13 16:22:57 +0700

    commit

--- main/rs485.c
+++ main/rs485.c
@@ -12,4 +12,18 @@
 void app_main(void)
 {
     xTaskCreate(RX_task, "RX_task", RX_TASK_STACK_SIZE, NULL, RX_TASK_PRIO, NULL);
+
+    float floatValue = -124.5; // Giá trị float ban đầu
+
+    unsigned char byteData[2]; // Mảng 2 byte để lưu giá trị float
+
+    floatToBytes(floatValue, byteData);
+    printf("Float value: %f\n", floatValue);
+    printf("Byte 0: 0x%02X\n", byteData[0]);
+    printf("Byte 1: 0x%02X\n", byteData[1]);
+
+    float convertedFloat = bytesToFloat(byteData);
+    printf("Converted Float value: %f\n", convertedFloat);
+
+    return 0;
 }
